c017374778efcd720f3cd18634824663e07f148e
Haruki N. Grande <22057153+HarukiGrande@users.noreply.github.com>
2018-05-07 18:50:43 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client["discordsites"]
+doc = db["guilds"]
+
+from flask import Flask, render_template, redirect
+application = Flask(__name__)
+application.config["PROPAGATE_EXCEPTIONS"] = True
+
+@application.route("/")
+def index():
+    return render_template("index.html")
+
+@application.route("/guild/<int:guild_id>/")
+def guild(guild_id):
+    if doc.find_one({"guild_id": guild_id}) is None:
+        return redirect("https://discordsites.me/", code=302)
+    info = doc.find_one({"guild_id": guild_id})
+    if info["toggle"] == 0:
+        return redirect("https://discordsites.me/", code=302)
+    guild_name = info["guild_name"]
+    guild_icon = info["guild_icon"]
+    guild_invite = info["guild_invite"]
+    guild_description = info["guild_description"]
+    guild_background = info["guild_background"]
+    guild_membercount = info["guild_membercount"]
+    return render_template("guild.html", guild_name=guild_name, guild_icon=guild_icon, guild_invite=guild_invite, guild_description=guild_description, guild_background=guild_background, guild_membercount=guild_membercount)
+
+if __name__ == "__main__":
+    application.run(host="0.0.0.0")
